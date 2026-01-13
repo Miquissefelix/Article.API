79e62b4e0af452f6c521ed3ec2cc6519fb36623c
MiquisseFelix <fmiquissejose@gmail.com>
2026-01-12 16:18:27 -0800

    Add AuthorName column to Comments via EF Core migration
    Introduced a new non-nullable AuthorName (text) column to the Comments table with a default value of an empty string.

---
+++ Infrastructure/Database/Migrations/20260112235045_SecondMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniDevTo.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorName",
+                table: "Comments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorName",
+                table: "Comments");
+        }
+    }
+}
